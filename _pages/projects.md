e12c80af120548b429cde9d14c3131018c1c20b4
Remy <shea.remy@gmail.com>
2019-08-18 17:13:00 -0700

    hosting jekyll site locally

--- _pages/projects.md
+++ _pages/projects.md
@@ -1,9 +1,8 @@
 ---
-layout: archives
+layout: archive
 title: "Projects"
 permalink: "/projects/"
 author_profile: true
 header:
   image: "/assets/images/vancouver_2_short.jpg"
 ---
-# TESTING PREAZE
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,16 @@
 var store = [{
         "title": "NBA Player Clustering",
-        "excerpt":"Dis a test   python code block:      import numpy as np     def test_function(x,y):       z = np.sum(x,y)       return z  Here ma face:    Here math:    ","categories": [],
+        "excerpt":"Clustering NBA Players through Unsupervised Learning Identifying player types and developing new metrics for evaluating professional basketball players. Remy Shea, May 2019 This was initially developed as my capstone project for the Data Science Immersive program at General Assembly Seattle. The code for this project can be seen on my...","categories": [],
         "tags": ["unsupervised learning","sports analytics","data science"],
         "url": "http://localhost:4000/nba-clustering/",
+        "teaser":null},{
+        "title": "Motion Detection in Dual-Mode Endomicroscopy",
+        "excerpt":"Motion Detection in DME Classifying motion in video data from a dual-mode endomicroscopic setup for early identification of precancerous developments in the oral epithelium. Remy Shea, BC Cancer Agency, February 2019 Introduction This was a project I had completed while working at the British Columbia Cancer Research Center, in the...","categories": [],
+        "tags": ["image processing","classification","biomedical optics"],
+        "url": "http://localhost:4000/dme-motion/",
+        "teaser":null},{
+        "title": "Subreddit Classification",
+        "excerpt":"Subreddit Classification Distinguishing between AITA and TIFU posts Remy Shea, April 2019 This was analysis was performed during my time at General Assembly Seattle’s Data Science Immersive program. The code for this project can be seen on my GitHub, here. Abstract In this project, the Reddit API was used to...","categories": [],
+        "tags": ["natural language processing","classification","data science"],
+        "url": "http://localhost:4000/subreddit-classification/",
         "teaser":null}]
